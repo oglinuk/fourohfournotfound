01b01abb3d992901aa1e9c1438e8568222d327e1
oglinuk <daniel-l-black@protonmail.com>
2021-12-17 10:15:06 -0800

    Update `generate.go`
    Added ability to remove all `.html` files, and ability to choose between
    generating or cleaning via `-c` flag.

--- generate.go
+++ generate.go
@@ -1,26 +1,53 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"sync"
+)
+
+var (
+	wg = &sync.WaitGroup{}
+
+	cmd = flag.String("c", "gen", "Command to run [gen/clean]")
 )
 
 func main() {
+	flag.Parse()
+
 	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
-		if !info.IsDir() && strings.HasSuffix(path, ".md") {
-			// TODO: Replace with PEGN-based parser
-			fmt.Printf("Rendering %s ...\n", path)
-			out := fmt.Sprintf("%s/index.html", filepath.Dir(path))
-			err = exec.Command("pandoc", path, "-o", out).Run()
-			if err != nil {
-				return err
+		switch strings.ToLower(*cmd) {
+		case "gen":
+			if !info.IsDir() && strings.HasSuffix(path, ".md") {
+				// TODO: Replace with PEGN-based parser
+				wg.Add(1)
+				go func() {
+					fmt.Printf("Rendering %s ...\n", path)
+					out := fmt.Sprintf("%s/index.html", filepath.Dir(path))
+					err = exec.Command("pandoc", path, "-o", out).Run()
+					if err != nil {
+						log.Println(err)
+					}
+					wg.Done()
+				}()
+			}
+		case "clean":
+			if !info.IsDir() && strings.HasSuffix(path, ".html") {
+				wg.Add(1)
+				go func() {
+					fmt.Printf("Removing %s ...\n", path)
+					os.Remove(path)
+					wg.Done()
+				}()
 			}
 		}
 		return nil
@@ -28,4 +55,5 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	wg.Wait()
 }
